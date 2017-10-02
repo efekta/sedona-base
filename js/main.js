04b13265722e0018a54f1c98787e7e7140e80872
efekta <efekta9@gmail.com>
2017-10-02 22:59:52 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -1,40 +1,19 @@
 jQuery(document).ready(function($) { 
-$(function() {
-  // This button will increment the value
   $('.qtyplus').click(function(e) {
-    // Stop acting like a button
-    e.preventDefault();
-    // Get the field name
-    fieldName = $(this).attr('field');
-    // Get its current value
-    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-    // If is not undefined
-    if (!isNaN(currentVal)) {
-      // Increment
-      $('input[name=' + fieldName + ']').val(currentVal + 1);
-    } else {
-      // Otherwise put a 0 there
-      $('input[name=' + fieldName + ']').val(0);
-    }
+    var $target = $(e.currentTarget),
+    $parent = $target.closest('.count-item-num'),
+    $num = $parent.find('.qty')
+    currentNum = parseInt($num.val(), 10) || 0;
+    $num.val(++currentNum);
   });
-  // This button will decrement the value till 0
-  $(".qtyminus").click(function(e) {
-    // Stop acting like a button
-    e.preventDefault();
-    // Get the field name
-    fieldName = $(this).attr('field');
-    // Get its current value
-    var currentVal = parseInt($('input[name=' + fieldName + ']').val());
-    // If it isn't undefined or its greater than 0
-    if (!isNaN(currentVal) && currentVal > 0) {
-      // Decrement one
-      $('input[name=' + fieldName + ']').val(currentVal - 1);
-    } else {
-      // Otherwise put a 0 there
-      $('input[name=' + fieldName + ']').val(0);
-    }
+
+  $('.qtyminus').click(function(e) {
+    var $target = $(e.currentTarget),
+    $parent = $target.closest('.count-item-num'),
+    $num = $parent.find('.qty')
+    currentNum = parseInt($num.val(), 10) || 1;
+    $num.val(--currentNum);
   });
-});
 
 
 
@@ -57,4 +36,10 @@ $( "#datepicker" ).datepicker({
     prevText: "<<"
   });
 
+});
+jQuery(document).ready(function($) { 
+var $modalBtn = $('.booking__button');
+$modalBtn.click(function() {
+$('.modal').slideToggle('slow');
+});
 });
