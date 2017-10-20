fe3a207b9b1b12be64362a86f995707188966586
efekta <efekta9@gmail.com>
2017-10-20 23:52:37 +0300

    no message

--- js/main.js
+++ js/main.js
@@ -15,10 +15,6 @@ jQuery(document).ready(function($) {
     $num.val(--currentNum);
   });
 
-
-
-
-
 $( "#datepicker" ).datepicker({
     showOn: "both",
     buttonImage: "img/icons/DE103C61.png",
@@ -35,11 +31,60 @@ $( "#datepicker" ).datepicker({
     nextText: ">>",
     prevText: "<<"
   });
-
-});
-jQuery(document).ready(function($) { 
 var $modalBtn = $('.booking__button');
 $modalBtn.click(function() {
 $('.modal').slideToggle('slow');
 });
+
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 3000,
+      values: [ 50, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "от " + ui.values[ 0 ] );
+        $( "#amount-max" ).val( "от " + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "от " + $( "#slider-range" ).slider( "values", 0 ));
+    $( "#amount-max" ).val( "от " + $( "#slider-range" ).slider( "values", 1 ));
+  });
+// Изменение местоположения ползунка при вводиде данных в первый элемент input
+      $("input#amount").change(function(){
+        var value1=$("input#amount").val();
+        var value2=$("input#amount-max").val();
+          if(parseInt(value1) > parseInt(value2)){
+          value1 = value2;
+          $("input#amount").val(value1);
+        }
+        $("#slider-range").slider("values",0,value1); 
+      });
+          
+      // Изменение местоположения ползунка при вводиде данных в второй элемент input  
+      $("input#amount-max").change(function(){
+        var value1=$("input#amount").val();
+        var value2=$("input#amount-max").val();
+
+        if(parseInt(value1) > parseInt(value2)){
+          value2 = value1;
+          $("input#amount-max").val(value2);
+        }
+        $("#slider-range").slider("values",1,value2);
+      });
+
+      // фильтрация ввода в поля
+      jQuery('#amount, #amount-max').keypress(function(event){
+        var key, keyChar;
+        if(!event) var event = window.event;
+        
+        if (event.keyCode) key = event.keyCode;
+        else if(event.which) key = event.which;
+      
+        if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+        keyChar=String.fromCharCode(key);
+        
+        if(!/\d/.test(keyChar)) return false;
+      
+      });
 });
